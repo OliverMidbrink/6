96c63d4510b1d91914d561c35e757f3725a9c3ed
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-15 10:27:40 +0100

    ml first sketch

---
+++ ml_main.py
@@ -0,0 +1,9 @@
+
+from ml.protein_interaction_model import ProteinInteractionModel
+
+def main():
+    protein_interaction_model = ProteinInteractionModel()
+    protein_interaction_model.summary()
+
+if __name__ == "__main__":
+    main()
