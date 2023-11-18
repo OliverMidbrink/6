4864e5eaa03f46fafb13b773d929d2c87dcf2734
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-18 17:09:37 +0100

    Cleaned run_model

--- graph_ml/run_model.py
+++ graph_ml/run_model.py
@@ -22,36 +22,8 @@ def main():
 
     x_train, y_train = load_data()
 
-    max_num_nodes = 0
-
-    # Assuming x_train contains your input data as a list of NumPy arrays
-    for data in x_train:
-        num_nodes = data.shape[0]
-        if num_nodes > max_num_nodes:
-            max_num_nodes = num_nodes
-
-    x_train_padded = []
-    for data in x_train:
-        num_nodes = data.shape[0]
-        padded_data = np.zeros((max_num_nodes, num_features))
-        padded_data[:num_nodes, :] = data
-        x_train_padded.append(padded_data)
-
-    # Convert sparse adjacency matrices to dense matrices and reshape them
-    adj_input_dense = []
-    for adj_matrix in adj_input:
-        dense_matrix = adj_matrix.toarray()  # Convert sparse matrix to dense
-        num_nodes = dense_matrix.shape[0]
-        padded_matrix = np.zeros((max_num_nodes, max_num_nodes))
-        padded_matrix[:num_nodes, :num_nodes] = dense_matrix
-        adj_input_dense.append(padded_matrix)
-
-    # Convert the lists to NumPy arrays
-    x_train_padded = np.array(x_train_padded)
-    adj_input_dense = np.array(adj_input_dense)
-
     # Train the model
-    history = model.fit([x_train_padded, adj_input_dense], y_train, epochs=1, batch_size=1)
+    history = model.fit(x_train, y_train, epochs=1, batch_size=1)
 
 if __name__ == "__main__":
     main()
