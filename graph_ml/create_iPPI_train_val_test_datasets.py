753893f8721ceae9851b1cf5b267a91db09632b1
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-21 04:01:27 +0100

    update

--- graph_ml/create_iPPI_train_val_test_datasets.py
+++ graph_ml/create_iPPI_train_val_test_datasets.py
@@ -1,6 +1,82 @@
 import json
 import os
 import random
+from scipy.sparse import csr_matrix
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_graph(G):
+    # The graph to visualize
+    G = nx.from_scipy_sparse_array(G)
+
+    # 3d spring layout
+    pos = nx.spring_layout(G, dim=3, seed=779)
+    # Extract node and edge positions from the layout
+    node_xyz = np.array([pos[v] for v in sorted(G)])
+    edge_xyz = np.array([(pos[u], pos[v]) for u, v in G.edges()])
+
+    # Create the 3D figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Plot the nodes - alpha is scaled by "depth" automatically
+    ax.scatter(*node_xyz.T, s=100, ec="w")
+
+    # Plot the edges
+    for vizedge in edge_xyz:
+        ax.plot(*vizedge.T, color="tab:gray")
+
+    _format_axes(ax)
+    fig.tight_layout()
+    plt.show()
+
+def _format_axes(ax):
+    """Visualization options for the 3D axes."""
+    # Turn gridlines off
+    ax.grid(False)
+    # Suppress tick labels
+    for dim in (ax.xaxis, ax.yaxis, ax.zaxis):
+        dim.set_ticks([])
+    # Set axes labels
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+
+
+
+def create_csr_graph(edge_list):
+    # Flatten the list of edges and determine unique nodes
+    nodes = set([node for edge in edge_list for node in edge])
+    # Create a mapping from node names to integers
+    node_to_idx = {node: idx for idx, node in enumerate(nodes)}
+    
+    # Initialize lists to store the CSR data
+    row_indices = []
+    col_indices = []
+    data = []
+    
+    # Populate the lists with edge data
+    for edge in edge_list:
+        row_indices.append(node_to_idx[edge[0]])
+        col_indices.append(node_to_idx[edge[1]])
+        data.append(1)  # Assuming an unweighted graph
+
+        # For undirected graphs, add the edge in the other direction
+        row_indices.append(node_to_idx[edge[1]])
+        col_indices.append(node_to_idx[edge[0]])
+        data.append(1)  # Assuming an unweighted graph
+
+    # Create the CSR matrix
+    graph_csr = csr_matrix((data, (row_indices, col_indices)), shape=(len(nodes), len(nodes)))
+    return graph_csr
+
+def get_uniprots(keys, DLiP_data):
+    proteins = set()
+    for key in keys:
+        for prot in DLiP_data[key]["proteins"]:
+            proteins.add(prot)
+    return proteins
 
 def main():
     output_dir_path = "data/mol_graphs"
@@ -13,23 +89,13 @@ def main():
     with open(molecule_smiles, "r") as file:
         DLiP_data = json.load(file)
     
-    uniprot_ids = list(set([prot for x in DLiP_data.values() for prot in x["proteins"]]))
-    random.shuffle(uniprot_ids)
+    edge_list = [x["proteins"] for x in DLiP_data.values()]
+    
+    csr_graph = create_csr_graph(edge_list)
 
-    # 1 divide the DLiP_data
-    keys = DLiP_data.keys()
-    random.shuffle(keys)
-
-
-    train_val_split = int(len(keys) * 0.8)
-    val_test_split = int(len(keys) * 0.9)
-    train_keys = keys[:train_val_split]
-    val_keys = keys[train_val_split:val_test_split]
-    test_keys = keys[val_test_split:]
-
-
-
-    # 2 divide all the overlaps
+    print(csr_graph)
+    
+    plot_graph(csr_graph)
 
     # Fill in with equal amount of assumed non iPPI prot pairs + mol
 
