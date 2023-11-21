ad26c376e55bdae2a0b722faaff98c88106d401c
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-21 15:14:23 +0100

    update

--- graph_ml/load_iPPI_data.py
+++ graph_ml/load_iPPI_data.py
@@ -1,9 +1,10 @@
 import h5py
 from scipy.sparse import csr_matrix
-from spektral.data import Graph
+from spektral.data import Graph, Dataset
 from tensorflow.keras.utils import Sequence
 import numpy as np
 import os
+from tqdm import tqdm
 
 def load_graph_from_hdf5(file_path):
     with h5py.File(file_path, 'r') as f:
@@ -25,6 +26,36 @@ def load_graph_from_hdf5(file_path):
 
     return graph
 
+
+class iPPIDataset(Dataset):
+    def __init__(self, mode, **kwargs):
+        self.mode = mode # mode is "train", "val" or "test"
+        if mode not in ["train", "val", "test"]:
+            print('mode has to be one of "train", "val" or "test"')
+            raise ValueError
+
+        super().__init__(**kwargs)
+
+
+    def read(self):
+        graphs = []
+
+        graph_dataset_file = ""
+        if self.mode == "train":
+            graph_dataset_file = "data/iPPI_graphs/train_graphs"
+        if self.mode == "val":
+            graph_dataset_file = "data/iPPI_graphs/val_graphs"
+        if self.mode == "test":
+            graph_dataset_file = "data/iPPI_graphs/test_graphs"
+
+
+        graph_files = sorted(os.listdir(graph_dataset_file))
+
+        for graph_file in tqdm(graph_files, desc="Loading {} files.".format(self.mode), unit="graphs"):
+            graph = load_graph_from_hdf5(os.path.join(graph_dataset_file, graph_file))
+            graphs.append(graph)
+
+        return graphs
 
 
 def train_generator():
--- graph_ml/run_iPPI_model.py
+++ graph_ml/run_iPPI_model.py
@@ -1,5 +1,7 @@
 from iPPI_prediction_model import create_iPPI_prediction_model
-from load_iPPI_data import train_generator
+from load_iPPI_data import iPPIDataset
+from spektral.data import DisjointLoader
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 def main():
     model = create_iPPI_prediction_model()
@@ -7,7 +9,23 @@ def main():
 
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=['accuracy'])
 
-    model.fit(train_generator())
+    train_dataset = iPPIDataset(mode="train")
+    val_dataset = iPPIDataset(mode="val")
+
+    train_loader = DisjointLoader(train_dataset, batch_size=4, epochs=100)
+    val_loader = DisjointLoader(val_dataset, batch_size=4)
+
+    checkpoint_path = "graph_ml/checkpoint_iPPI_model.keras"
+    checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+
+    model.fit(
+        train_loader.load(),
+        validation_data=val_loader.load(),
+        validation_steps=val_loader.steps_per_epoch,
+        steps_per_epoch=train_loader.steps_per_epoch,
+        epochs=100,
+        callbacks=[checkpoint] 
+    )
 
 if __name__ == "__main__":
     main()
