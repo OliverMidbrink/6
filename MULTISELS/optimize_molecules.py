81523bb137b8e1348d35c3dd92b11680d588814e
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-21 23:16:12 +0100

    Test

--- MULTISELS/optimize_molecules.py
+++ MULTISELS/optimize_molecules.py
@@ -6,6 +6,8 @@ from scipy.sparse import csr_matrix
 from spektral.data import Graph
 import numpy as np
 from iPPI_predict import get_model, predict_from_uniprots_and_smiles
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def get_targets_iPPIs(file_path):
     with open(file_path, "r") as file:
@@ -33,18 +35,44 @@ def load_graph(file_path):
     return csr_graph, features
 
 def main():
-    model = get_model()
     tuples = get_targets_iPPIs("MULTISELS/OSK_upreg_2_neighbors_chatGPT3_turpo_1106.json")
     
+    if os.path.exists("MULTISELS/smile_scores.json"):
+        smile_scores = []
+        with open("MULTISELS/smile_scores.json", "r") as file:
+            smile_scores = json.load(file).values()
+
+
+        plt.plot([data["total_score"] for data in smile_scores])
+        plt.show()
+        return None
+
+    model = get_model()
     smiles_subset = random.sample(get_smiles(), 100)
 
-    for smile in smiles_subset:
+    smile_scores = {}
+    id = 0
+    for smile in tqdm(smiles_subset, desc="Getting smile scores", unit="smiles"):
+        smile_score = 0
         for tuple in tuples:
             iPPI_prob = predict_from_uniprots_and_smiles(tuple[0], tuple[1], smile, model)
             if iPPI_prob is not None:
                 iPPI_prob = iPPI_prob[0][0]
                 print(iPPI_prob)
-    for x in tuples:
-        pass
+            
+                smile_score += iPPI_prob * tuple[2]
+                print("Added {} to score.".format(iPPI_prob * tuple[2]))
+            else:
+                pass
+                #smile_score += 0.5 * tuple[2]
+                #print("Assumed 0.5 iPPI_prob and added {} to score.".format(0.5 * tuple[2]))
+        
+        smile_scores[id] = {"smile": smile, "total_score": smile_score}
+        id += 1
+
+    with open("MULTISELS/smile_scores.json", "w") as file:
+        json.dump(smile_scores, file)
+    
+
 if __name__ == "__main__":
     main()
