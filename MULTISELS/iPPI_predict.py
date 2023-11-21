c19938ef3e4a087d58b82d5f1c204b0db88a0202
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-21 16:10:32 +0100

    Predict function

---
+++ MULTISELS/iPPI_predict.py
@@ -0,0 +1,63 @@
+from tensorflow.keras.models import load_model
+from spektral.layers import GCNConv, GlobalAvgPool
+import h5py
+import os
+import h5py
+from scipy.sparse import csr_matrix
+from spektral.data import DisjointLoader, Graph, Dataset
+import numpy as np
+import os
+from tqdm import tqdm
+
+class PredDataset(Dataset):
+    def __init__(self, graphs, **kwargs):
+        self.graphs = graphs
+        super().__init__(**kwargs)
+
+    def read(self):
+        return self.graphs
+
+def load_graph_from_hdf5(file_path):
+    with h5py.File(file_path, 'r') as f:
+        # Load the features matrix
+        features = f['features'][:]
+
+        # Load the CSR components and reconstruct the adjacency matrix
+        data = f['data'][:]
+        indices = f['indices'][:]
+        indptr = f['indptr'][:]
+        shape = f['shape'][:]
+        adjacency = csr_matrix((data, indices, indptr), shape=shape)
+
+        # Load the labels or targets if they exist
+        label = f['labels'][:] if 'labels' in f else None
+
+    # Create the Spektral Graph object
+    graph = Graph(x=features, a=adjacency, y=label)
+
+    return graph
+
+
+def predict(graphs):
+    model = load_model("graph_ml/checkpoint_iPPI_model.h5", custom_objects={'GCNConv': GCNConv, 'GlobalAvgPool': GlobalAvgPool})
+    
+    dataset = PredDataset(graphs=graphs)
+    loader = DisjointLoader(dataset, batch_size=1, epochs=1)
+
+    y = model.predict(loader.load())
+
+    return y
+
+
+def main():
+    graphs = []
+    file_name = "data/iPPI_graphs/train_graphs/"
+    for file in sorted(list(os.listdir(file_name)))[:100]:
+        graph = load_graph_from_hdf5(os.path.join(file_name, file))
+        graphs.append(graph)
+
+    y = predict(graphs)
+    print(y)
+
+if __name__ == "__main__":
+    main()
