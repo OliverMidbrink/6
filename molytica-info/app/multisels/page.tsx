fe18e345bbbad66aef6b2d810dfc285314d7a8f0
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-22 05:47:04 +0100

    update

--- molytica-info/app/multisels/page.tsx
+++ molytica-info/app/multisels/page.tsx
@@ -16,7 +16,7 @@ export default function Multisels() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Molytica
+            Molytica{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -33,7 +33,7 @@ export default function Multisels() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Contact
+            Contact{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
