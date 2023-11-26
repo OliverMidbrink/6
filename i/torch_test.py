491ef326fbeb4e2022a6951d2c7f78f233ef1761
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-26 23:15:40 +0100

    update

--- i/torch_test.py
+++ i/torch_test.py
@@ -8,6 +8,21 @@ import os
 import numpy as np
 from torch_geometric.nn import GCNConv
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
+
+def run_timestep_model(node_features, edge_index, edge_features):
+    
+    neighbor_sum = torch.zeros_like(node_features).to(device)
+    for i in range(edge_index.size(1)):
+        src, dst = edge_index[0, i], edge_index[1, i]
+        neighbor_sum[dst] += node_features[src] * edge_features[i]
+
+    # Update node features
+    new_node_features = node_features + neighbor_sum
+
+    return new_node_features, edge_index, edge_features
+
 def get_HuRI_table_as_edge_list():
     if os.path.exists("i/HuRI_ENSG_EDGELIST.json"):
         edge_list = []
@@ -39,17 +54,17 @@ def get_HuRI_graph():
     mean, std_dev = 0, 1  # Mean and standard deviation for normal distribution
 
     for node in G.nodes():
-        G.nodes[node]['features'] = np.random.normal(mean, std_dev, 2)
+        G.nodes[node]['features'] = np.random.normal(mean, std_dev, 1)
 
     # Assign random features to edges
     for edge in G.edges():
-        G.edges[edge]['features'] = np.random.normal(mean, std_dev, 2)
+        G.edges[edge]['features'] = np.random.normal(mean, std_dev, 1)
 
     return G
 
 def to_pytorch_from_nx(G):
     # Convert node features to a tensor
-    node_features = torch.tensor(np.array([G.nodes[node]['features'] for node in G.nodes()]))
+    node_features = torch.tensor(np.array([G.nodes[node]['features'] for node in G.nodes()]), dtype=float).to(device)
 
     # Convert edges to a tensor (edge index)
     edge_list = [(u, v) for u, v in G.edges()]
@@ -61,10 +76,10 @@ def to_pytorch_from_nx(G):
     # Convert edge list to numeric indices
     numeric_edge_list = [(node_mapping[node1], node_mapping[node2]) for node1, node2 in edge_list]
 
-    edge_index = torch.tensor(numeric_edge_list, dtype=torch.long).t().contiguous()
+    edge_index = torch.tensor(numeric_edge_list, dtype=torch.long).t().contiguous().to(device)
 
     # If your graph has edge features
-    edge_features = torch.tensor(np.array([G[u][v]['features'] for u, v in G.edges()]))
+    edge_features = torch.tensor(np.array([G[u][v]['features'] for u, v in G.edges()]), dtype=float).to(device)
 
     #print("Node Features:\n", node_features)
     #print("Edge Index:\n", edge_index)
@@ -75,7 +90,11 @@ def to_pytorch_from_nx(G):
 
 def main():
     graph = get_HuRI_graph()
-    pytorch_tensor_graph = to_pytorch_from_nx(graph)
+    node_features, edge_index, edge_features = to_pytorch_from_nx(graph)
+
+
+    node_features, edge_index, edge_features = run_timestep_model(node_features, edge_index, edge_features)
+
 
 if __name__ == "__main__":
     main()
