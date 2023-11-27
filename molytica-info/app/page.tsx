06ca710825a8b0f2c52f6dd08f1dd97f96dd1054
Oliver Midbrink <39882842+OliverMidbrink@users.noreply.github.com>
2023-11-27 18:54:06 +0100

    Update page.tsx

--- molytica-info/app/page.tsx
+++ molytica-info/app/page.tsx
@@ -26,24 +26,6 @@ export default function Home() {
             Complex Diseases Require Complex Solutions.
           </p>
         </Link>
-
-        <Link
-          href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Contact{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            oliver.midbrink@gmail.com
-          </p>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            0763271528
-          </p>
-        </Link>
       </div>
     </main>
   )
