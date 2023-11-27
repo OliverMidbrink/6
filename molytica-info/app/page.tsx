2792c03eea39b5f053c27cfda617989d94bb23af
Oliver Midbrink <oliver.midbrink@hotmail.com>
2023-11-27 04:52:26 +0100

    update

--- molytica-info/app/page.tsx
+++ molytica-info/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
         <Link
-          href="/multisels"
+          href="/m"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
